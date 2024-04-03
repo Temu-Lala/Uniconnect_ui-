3a06ed4af00bff3c9d3a21744edfee94afcaf556
Temu Lala <temesgendebebe1921@gmail.com>
2024-04-03 17:30:29 +0300

    uNI CENNECT

---
+++ app/(Pages)/Error/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Errors from '../../Components/Error/Error403/page'
+
+export default function page() {
+  return (
+    <div className= " flex W-screen h-screen text-white "> <Errors/> </div>
+   )
+}
